fcef0de04e159a9c037221c7c299aa3351fd3a66
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-07-05 16:43:55 +0900

    코어 모듈

--- Node.js/main.js
+++ Node.js/main.js
@@ -1,7 +1,3 @@
-let m = require('./math.tools.js');
+const os = require('os');
 
-console.log(m.PI);
-console.log(m.add(2, 4));
-console.log(m.subtract(1, 2));
-console.log(m.multiply(1, 2));
-console.log(m.divide(1, 2));
+console.log(os.cpus());
