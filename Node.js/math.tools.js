23b90ec900c4376e4258b17d52472e21f67770b9
SU KYUNG IM <99783474+oiosu@users.noreply.github.com>
2023-07-05 16:10:35 +0900

    하나의 객체로 모아서 외부에 공개하기

--- Node.js/math.tools.js
+++ Node.js/math.tools.js
@@ -1,5 +1,9 @@
-function add(a, b) {
-    return a + b;
-}
+let calculator = {
+    PI: 3.14,
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+};
 
-exports.add = add;
+module.exports = calculator; 
